f6baaea2661489d791e326d7f0a49e89db872429
JimFung <jfung92@gmail.com>
2015-03-15 17:11:13 -0700

    Deceleration almost done

--- Prodigy/Assets/Script/DroneEvent.cs
+++ Prodigy/Assets/Script/DroneEvent.cs
@@ -5,10 +5,14 @@ public class DroneEvent : MonoBehaviour {
 
 	private static bool trigger;
 	private static Rigidbody droneRB;
-	private float accel = 5f;
+	private static bool decelerate;
+	private float accel = 10f;
+	private float decel = -10f;
+	private float stop = 0f;
 
 	void Start(){
 		trigger = false;
+		decelerate = false;
 		droneRB = GetComponent<Rigidbody>();
 	}
 
@@ -16,7 +20,23 @@ public class DroneEvent : MonoBehaviour {
 		if (trigger) {
 			//change z vector for droneRB;
 			droneRB.AddForce(Vector3.back * accel, ForceMode.Acceleration);
+			Debug.Log(droneRB.velocity.magnitude);
+			if(droneRB.velocity.magnitude >= 8.0f){
+				trigger = false;
+				decelerate = true;
+				accel *= -1;
+			}
+		}
+		if (decelerate) {
+			//code to decelerate
+			Debug.Log("Hi!");
 			droneRB.useGravity = true;
+			droneRB.AddForce(Vector3.back * decel, ForceMode.Impulse);
+			Debug.Log(droneRB.velocity.magnitude);
+			if(droneRB.velocity.magnitude <= 0.0f){
+
+				decelerate = false;
+			}
 		}
 	}
 
