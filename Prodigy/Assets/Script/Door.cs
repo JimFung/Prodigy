92d3eac8c28fdc458d5947ac7a8b3388111d02a7
JimFung <jfung92@gmail.com>
2015-03-17 19:03:41 -0700

    fixed door race condition

--- Prodigy/Assets/Script/Door.cs
+++ Prodigy/Assets/Script/Door.cs
@@ -5,18 +5,21 @@ public class Door : MonoBehaviour {
 
 	private Animator animator;
 	private bool doorOpen;
+	private bool doorlock;
 
 	//boilerplate
 	void Start(){
 		doorOpen = false;
 		animator = GetComponent<Animator>();
+		doorlock = true;
 	}
 
 	//open door
 	void OnTriggerEnter(Collider obj){
-		if (obj.gameObject.name == "Drone" || obj.gameObject.name == "Head") {
+		if ((obj.gameObject.name == "Drone" || obj.gameObject.name == "Head") && doorlock) {
 			doorOpen = true;
 			DoorController("Open");
+			doorlock = false;
 		}
 	}
 
@@ -25,12 +28,13 @@ public class Door : MonoBehaviour {
 	//components attached to the camera that would trigger this method call.
 	void OnTriggerExit(Collider obj){
 
-		//this check makes sure that we only send the Close trigger once per open.
+			//this check makes sure that we only send the Close trigger once per open.
 		if (doorOpen && (obj.gameObject.name == "Drone" || obj.gameObject.name == "Head")) {
 			doorOpen = false;
-			DoorController("Close");
+			doorlock = true;
+			DoorController ("Close");
+			
 		}
-
 	}
 
 	//Passes the state we want to the state diagram in the animator
