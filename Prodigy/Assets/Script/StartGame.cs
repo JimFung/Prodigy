76d16c6efc59be1acbd252a367cda8b92e1ebe9a
calthom <caledoniathomson@gmail.com>
2015-03-20 06:33:23 -0700

    Menu loading, replay, button trigger end game

--- Prodigy/Assets/Script/StartGame.cs
+++ Prodigy/Assets/Script/StartGame.cs
@@ -4,21 +4,21 @@ using System.Collections;
 public class StartGame : MonoBehaviour {
 	
 	private CardboardHead head;
-	private Vector3 startingPosition;
-	private float delay = 0.0f; 
 	public Color greyBlue = new Color(0.2F, 0.3F, 0.4F, 0.5F);
+	public Color lightBlue = new Color(0.1F, 0.15F, 0.2F, 0.5F);
 	
 	void Start() {
 		head = Camera.main.GetComponent<StereoController>().Head;
-		startingPosition = transform.localPosition;
 	}
 	
 	void Update() {
 		RaycastHit hit;
 		bool isLookedAt = GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity);
-		if (isLookedAt && Time.time>delay  && Cardboard.SDK.CardboardTriggered) { 
+		if (isLookedAt && Cardboard.SDK.CardboardTriggered) { 
+			GetComponent<TextMesh>().text = "LOADING";
+			GetComponent<Renderer>().material.color = lightBlue;
 			Application.LoadLevel("Game");
-			delay = Time.time + 2.0f;
+
 		}
 		// currently looking at object
 		else if (isLookedAt) { 
@@ -27,7 +27,6 @@ public class StartGame : MonoBehaviour {
 		// not looking at object
 		else if (!isLookedAt) { 
 			GetComponent<Renderer>().material.color = Color.white; 
-			delay = Time.time + 2.0f; 
 		}
 	}
 	
